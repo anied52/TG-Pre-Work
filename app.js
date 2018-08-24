c7e16b2490fe6478aaba2fc66fb7553e5f05fdf3
anied52 <aniederauer@msn.com>
2018-08-24 13:37:30 -0600

    Mathematical assignment operators

--- app.js
+++ app.js
@@ -74,3 +74,17 @@ let notDefined;
 let valueless;
 console.log(valueless);
 
+// NATHEMATICAL ASSIGNMENT OPERATORS
+let molecule = 16;
+let particle = 18;
+let assay = 3;
+
+// Add and assign to molecule below
+molecule += 16;
+
+// Multiply and assign to particle below
+particle *= 6.02;
+
+// Increment assay by 1
+assay++;
+
