9bcd41534df0ee47ead580c3934921c87e6e9496
anied52 <aniederauer@msn.com>
2018-08-24 13:11:47 -0600

    Math Operators

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ console.log('JavaScript');
 console.log(33.7);
 console.log(true);
 console.log(null);
+
+console.log(66 + 3.5);
+console.log(2018 - 1969);
+console.log(65 / 240);
+console.log(0.2708 * 100);
