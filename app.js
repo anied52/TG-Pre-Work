f3960fea11350900b091226022b396e912aa1189
anied52 <aniederauer@msn.com>
2018-08-25 01:37:27 -0600

    Return II

--- app.js
+++ app.js
@@ -329,3 +329,27 @@ takeOrder('pepperoni', 'brooklyn style');
 
 console.log(getSubTotal(orderCount));
 
+// RETURN II
+let orderCount = 0;
+
+const takeOrder = (topping, crustType) => {
+  orderCount++;
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+};
+
+takeOrder('mushroom', 'thin crust');
+takeOrder('spinach', 'whole wheat');
+takeOrder('pepperoni', 'brooklyn style');
+
+const getSubTotal = (itemCount) => {
+  return itemCount * 7.5;
+};
+
+const getTax = (itemCount) => {
+  return getSubTotal(itemCount) * 0.06;
+}
+
+const getTotal = (itemCount) => {
+  return getSubTotal(itemCount) + getTax(itemCount);
+}
+console.log(getTotal(orderCount));
