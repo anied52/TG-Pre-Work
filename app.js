eaa365360d8c18c51c89bce7e2dbed9b338ee5a0
anied52 <aniederauer@msn.com>
2018-08-25 01:33:05 -0600

    Return

--- app.js
+++ app.js
@@ -311,3 +311,21 @@ takeOrder('mushrooms', 'thin');
 takeOrder('Pepperoni', 'original');
 takeOrder('sausage', 'pan');
 
+// RETURN
+const takeOrder = (topping, crustType) => {
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+  orderCount++;
+}
+
+let getSubTotal = (itemCount) => {
+	return itemCount * 7.5;  
+}
+
+let orderCount = 0;
+
+takeOrder('mushroom', 'thin crust');
+takeOrder('spinach', 'whole wheat');
+takeOrder('pepperoni', 'brooklyn style');
+
+console.log(getSubTotal(orderCount));
+
