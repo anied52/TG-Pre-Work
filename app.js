7e51aabd8ae819b852c02c5e1119926cf31cd0ef
anied52 <aniederauer@msn.com>
2018-08-25 00:40:49 -0600

    True or false values II

--- app.js
+++ app.js
@@ -180,3 +180,22 @@ if (favoritePhrase) {
   console.log('This string is definitely empty.');
 }
 
+// TRUE AND FALSE VALUES II
+let wordCount = 1;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = 'xyz';
+
+if (!favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
+  
