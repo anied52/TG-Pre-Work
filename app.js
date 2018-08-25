358c6535e503002ac3356a28d36159f5e3deeffb
anied52 <aniederauer@msn.com>
2018-08-25 00:53:49 -0600

    Logical Operators

--- app.js
+++ app.js
@@ -226,3 +226,16 @@ if (moonPhase === 'full') {
   console.log('Invalid moon phase');
 }
 
+// LOGICAL OPERATORS
+let moonPhase = 'full';
+let isFoggyNight = false;
+if (moonPhase === 'full' || isFoggyNight) {
+  console.log('Howl!');
+} else if (moonPhase === 'mostly full') {
+  console.log('Arms and legs are getting hairier');
+} else if (moonPhase === 'mostly new') {
+  console.log('Back on two feet');
+} else {
+  console.log('Invalid moon phase');
+}
+
