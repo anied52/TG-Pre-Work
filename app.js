df73143ec2379d8dfc1267fb47dba33708b9374f
anied52 <aniederauer@msn.com>
2018-08-25 13:52:14 -0600

    Block scope III

--- app.js
+++ app.js
@@ -416,3 +416,17 @@ const visibleLightWaves = function () {
 visibleLightWaves();
 console.log(lightWaves);
 
+// BLOCK SCOPE II
+const visibleLightWaves = function () {
+  let lightWaves = 'Moonlight';
+  let region = 'The Arctic';
+  
+  if (region === 'The Arctic') {
+    let lightWaves = 'Northern Lights';
+    console.log(lightWaves);
+  }
+  console.log(lightWaves);
+}
+
+visibleLightWaves();
+
