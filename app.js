69e29ff2308a3c63a082036a60f297f2ef0af86b
anied52 <aniederauer@msn.com>
2018-08-25 13:16:03 -0600

    Arrow functions

--- app.js
+++ app.js
@@ -376,3 +376,9 @@ const isGreaterThan = (numberOne, numberTwo) => {
 
 isGreaterThan(4, 8);
 
+// ARROW FUNCTIONS
+const volumeOfSphere = diameter => (1/6) * Math.PI * diameter * diameter * diameter;
+
+
+console.log('The volume of a sphere is ' + volumeOfSphere(10) + ' cubic centimeters');
+
