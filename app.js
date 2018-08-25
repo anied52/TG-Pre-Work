20b8c98bdce8f76a6f1ebf0333b69263b285d673
anied52 <aniederauer@msn.com>
2018-08-25 00:35:30 -0600

    Control flow introduction

--- app.js
+++ app.js
@@ -140,3 +140,17 @@ let myName = 'Alan'.toLowerCase();
 // print my name and age in dog years
 console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
 
+// CONTROL FLOW INTRODUCTION
+let userName = 'Alan';
+let knowsJavaScript = true;
+
+if (knowsJavaScript && userName) {
+  console.log('Great, ' + userName + '! Get ready to practice your JavaScript!');
+} else if (knowsJavaScript) {
+  console.log('Great! Get ready to practice your JavaScript!');
+} else if (userName) {
+  console.log('Great, ' + userName + '! Get ready to learn something new!');
+} else {
+  console.log('Great! Get ready to learn something new!');
+}
+
