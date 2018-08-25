fec72b8309d2559d06c269a05d91680b0a83d69b
anied52 <aniederauer@msn.com>
2018-08-25 13:54:17 -0600

    Arrays

--- app.js
+++ app.js
@@ -430,3 +430,15 @@ const visibleLightWaves = function () {
 
 visibleLightWaves();
 
+// ARRAYS
+const starCount = function () {
+	let i = 5;
+  console.log(i);
+  for (let i = 0; i < 12; i++) {
+    console.log(i);
+  }
+}
+
+starCount();
+console.log(i);
+
