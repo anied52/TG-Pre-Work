c532a0a86f9800d57de7cdbd75c32e786d22ce49
anied52 <aniederauer@msn.com>
2018-08-25 00:53:29 -0600

    else/if statements

--- app.js
+++ app.js
@@ -214,3 +214,15 @@ if (moonPhase === 'full') {
   console.log('I swear I am not a werewolf');
 }
 
+// ELSE IF STATEMENTS
+let moonPhase = 'solar eclipse';
+if (moonPhase === 'full') {
+  console.log('Howl!');
+} else if (moonPhase === 'mostly full') {
+  console.log('Arms and legs are getting hairier');
+} else if (moonPhase === 'mostly new') {
+  console.log('Back on two feet');
+} else {
+  console.log('Invalid moon phase');
+}
+
