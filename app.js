aef44de68de5ecb2d5f4104a1e3db16822560220
anied52 <aniederauer@msn.com>
2018-08-26 17:01:53 -0600

    Constructor

--- app.js
+++ app.js
@@ -990,3 +990,11 @@ halley.incrementBehavior(); // Add one to behavior
 console.log(halley.name); // Print name value to console
 console.log(halley.behavior); // Print behavior value to console
 
+// CONSTRUCTOR
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+
