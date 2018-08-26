b01d5957c476fd0724d2e971ab5f6077f8ad543a
anied52 <aniederauer@msn.com>
2018-08-26 01:55:26 -0600

    .map()

--- app.js
+++ app.js
@@ -617,3 +617,18 @@ fruits.forEach(function(fruit) {
   console.log('I want to eat a ' + fruit);
 });
 
+// .MAP()
+let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+let secretMessage = animals.map(animals => animals[0]);
+
+console.log(secretMessage.join(''));
+
+let bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+let smallNumbers = bigNumbers.map(function(number) {
+  return number / 100;
+});
+
