87907362dd0c64d2610e26c8a700799dabbb3f1a
anied52 <aniederauer@msn.com>
2018-08-26 17:06:31 -0600

    Instance

--- app.js
+++ app.js
@@ -998,3 +998,16 @@ class Surgeon {
   }
 }
 
+// INSTANCE
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+
+
