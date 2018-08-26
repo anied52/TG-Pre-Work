ef361b44948dc84dc80f711a795587cf321b3e0e
anied52 <aniederauer@msn.com>
2018-08-26 17:11:03 -0600

    Methods

--- app.js
+++ app.js
@@ -1010,4 +1010,31 @@ const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
 
 const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
 
+// METHODS
+class Surgeon {
+  constructor(name, department) {
+    this._name = name;
+    this._department = department;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get department() {
+    return this._department;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+}
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
 
