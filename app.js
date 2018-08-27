f1c955b799a33fb9cc8455c28b6378bebcd3ee45
anied52 <aniederauer@msn.com>
2018-08-26 22:32:06 -0600

    Requires()

--- app.js
+++ app.js
@@ -1260,3 +1260,12 @@ Airplane.myAirplane = 'StarJet';
 
 module.exports = Airplane;
 
+// REQUIRES
+const Airplane = require('./1-airplane.js');
+
+function displayAirplane() {
+  console.log(Airplane.myAirplane);
+}
+
+displayAirplane();
+
