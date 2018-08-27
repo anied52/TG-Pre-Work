880b2ac300a3ee8b8b737fb0fefd4f38e96e70d9
anied52 <aniederauer@msn.com>
2018-08-27 15:41:54 -0600

    fetch POST requests II

--- app.js
+++ app.js
@@ -1835,3 +1835,18 @@ const displaySuggestions = (event) => {
 
 submit.addEventListener('click', displaySuggestions);
 
+// FETCH() POST REQUEST II
+fetch('https://api-to-call.com/endpoint', {
+  method: 'POST',
+  body: JSON.stringify({id: '200'})
+}) .then(response => {
+  	if (response.ok === response.DONE) {
+      return response.json();
+    }
+    throw new Error('Request failed!');;
+	}, networkError => {
+  		console.log(networkError.message);
+}) .then (jsonResponse => {
+  return jsonResponse;
+})
+
