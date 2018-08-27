649718b47a18575342af1438e4dc38bbd3fdfe3f
anied52 <aniederauer@msn.com>
2018-08-26 23:53:26 -0600

    XHR GET Requests II

--- app.js
+++ app.js
@@ -1573,3 +1573,17 @@ function displaySpeedRangeStatus() {
 
 displaySpeedRangeStatus();
 
+// XHR GET REQUESTS II
+const xhr = new XMLHttpRequest();
+const url = 'https://api-to-call.com/endpoint';
+
+xhr.responseType = 'json';
+xhr.onreadystatechange = () => {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    return xhr.response;
+  }
+};
+
+xhr.open('GET', url);
+xhr.send();
+
