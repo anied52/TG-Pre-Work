4ce31ba84b9107587eda7f9d633c7445cf56c9ef
anied52 <aniederauer@msn.com>
2018-08-26 23:45:28 -0600

    Combining Import statements

--- app.js
+++ app.js
@@ -1545,4 +1545,31 @@ export let availableAirplanes = [
   
   export default meetsSpeedRangeRequirements;
 
-  
+// COMBINING IMPORT STATEMENTS
+import {availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
+import meetsSpeedRangeRequirements from './airplane';
+
+function displayFuelCapacity() {
+  availableAirplanes.forEach(function(element) {
+    console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  });
+}
+
+displayFuelCapacity();
+
+function displayStaffStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+  });
+}
+
+displayStaffStatus();
+
+function displaySpeedRangeStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets speed range requirements:' + meetsSpeedRangeRequirementss(element.maxSpeed, element.minSpeed, flightRequirementss.requiredSpeedRange));
+  });
+}
+
+displaySpeedRangeStatus();
+
