090658be468de9aa74bc56ea99630a78d23084f4
anied52 <aniederauer@msn.com>
2018-08-26 23:00:50 -0600

    Named exports

--- app.js
+++ app.js
@@ -1307,5 +1307,42 @@ function displayFuelCapacity() {
 displayFuelCapacity();
 
 
+// NAMED EXPORTS
+let availableAirplanes = [
+  {
+    name: 'AeroJet',
+    fuelCapacity: 800,
+    availableStaff: [
+      'pilots',
+      'flightAttendants',
+      'engineers',
+      'medicalAssistance',
+      'sensorOperators'
+    ]
+  },
+  {
+    name: 'SkyJet',
+    fuelCapacity: 500,
+    availableStaff: [
+      'pilots',
+      'flightAttendants'
+    ]
+  }
+];
+
+let flightRequirements = {
+requiredStaff: 4
+};
+
+function meetsStaffRequirements(availableStaff, requiredStaff) {
+if (availableStaff.length >= requiredStaff) {
+  return true;
+} else {
+  return false;
+}
+}
+
+export {availableAirplanes, flightRequirements, meetsStaffRequirements };
+
 
 
