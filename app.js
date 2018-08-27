57c171aed098cf6878c43f7898599644b8454c33
anied52 <aniederauer@msn.com>
2018-08-27 15:55:27 -0600

    fetch() POST requests III

--- app.js
+++ app.js
@@ -1850,3 +1850,38 @@ fetch('https://api-to-call.com/endpoint', {
   return jsonResponse;
 })
 
+// FETCH() POST REQUESTS III
+// Information to reach API
+const apiKey = 'e4c7637ff159413d944a3cb0cb11c385';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  fetch(url, {
+    method: 'POST',
+    headers: {
+  		'Content-type': 'application/json',
+  		'apikey': apiKey,
+		},
+    body: data
+  });
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild)
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
