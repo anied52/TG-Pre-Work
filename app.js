5ca534908c57ee0386248de115c6d777085b4495
anied52 <aniederauer@msn.com>
2018-08-26 19:19:07 -0600

    Browser compatibility and transpilation introduction

--- app.js
+++ app.js
@@ -1237,3 +1237,13 @@ console.log(nurseOlynyk.remainingVacationDays);
 nurseOlynyk.addCertification('Genetics');
 console.log(nurseOlynyk.certifications);
 
+// BROWSER COMPATIBILITY AND TRANSPILATION - INTRODUCTION
+var pasta = "Spaghetti"; // ES5 syntax
+
+const meat = "Pancetta"; // ES6 syntax
+
+let sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
+
