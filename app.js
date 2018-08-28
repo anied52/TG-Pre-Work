30b6a58964a2a92a3fc76af8fc5050e8f46f488b
anied52 <aniederauer@msn.com>
2018-08-27 23:59:32 -0600

    async POST requests II

--- app.js
+++ app.js
@@ -2026,3 +2026,19 @@ const displaySuggestions = (event) => {
 
 submit.addEventListener('click', displaySuggestions);
 
+// ASYNC POST REQUESTS II
+const getData = async () => {
+  try {
+    const response = await fetch('https://api-to-call.com/endpoint', {
+      method: 'POST',
+      body: JSON.stringify({id: 200})
+    })
+    if(response.ok){
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error('Request failed!');
+  } catch(error) {
+    console.log(error);
+  }
+}
